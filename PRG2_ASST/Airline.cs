662bf19aae6c8d8fc5e2940be1b4f4dad947532a
Camellia Ong <cam.ongjs@gmail.com>
2025-01-16 00:12:31 +0800

    created and done up airline, terminal and boardinggate classes

---
+++ PRG2_ASST/Airline.cs
@@ -0,0 +1,56 @@
+﻿namespace PRG2_ASST
+{
+    class Airline
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public Dictionary<string, Flight> Flights { get; set; } = new Dictionary<string, Flight>();
+
+        public Airline() { }
+        public Airline(string n, string c)
+        {
+            Name = n;
+            Code = c;
+        }
+
+        public bool AddFlight(Flight flight)
+        { 
+            Flights[flight.FlightNumber] = flight;
+            if (Flights.ContainsKey(flight.FlightNumber) == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public double CalculateFees()
+        {
+            double fees = 0;
+            foreach (KeyValuePair<string, Flight> kvp in Flights)
+            {
+                fees += kvp.Value.CalculateFees();
+            }
+            return fees;
+        }
+        public bool RemoveFlight(Flight flight)
+        {
+            Flights.Remove(flight.FlightNumber);
+            if (Flights.ContainsKey(flight.FlightNumber) == false)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public override string ToString()
+        {
+            return "";
+          
+        }
+
+    }
+}
---
+++ PRG2_ASST/Terminal.cs
@@ -0,0 +1,67 @@
+﻿namespace PRG2_ASST
+{
+    class Terminal
+    {
+        public string TerminalName {  get; set; }
+        public Dictionary<string, Airline> Airlines { get; set; } 
+            = new Dictionary<string, Airline>();
+        public Dictionary<string, Flight> Flights { get; set; } 
+            = new Dictionary<string, Flight>();
+        public Dictionary<string, BoardingGate> BoardingGates { get; set; } 
+            = new Dictionary<string, BoardingGate>();
+        public Dictionary<string, double> GateFees { get; set; }
+            = new Dictionary<string, double>();
+
+        public Terminal() { }
+        public Terminal(string tN)
+        {
+            TerminalName = tN;
+        }
+
+        public bool AddAirline(Airline A)
+        {
+            Airlines[A.Code] = A;
+            if (Airlines.ContainsKey(A.Code) == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool AddBoardingGate(BoardingGate BG)
+        {
+            BoardingGates[BG.GateName] = BG;
+            if (BoardingGates.ContainsKey(BG.GateName) == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public Airline GetAirlineFromFlight(Flight f)
+        {
+            foreach (KeyValuePair<string, Airline> kvp in Airlines)
+            {
+                foreach (KeyValuePair<string, Flight> kvp2 in kvp.Value.Flights)
+                {
+                    if (kvp2.Value.FlightNumber == f.FlightNumber)
+                    {
+                        return kvp.Value;
+                    }
+                }
+            }
+
+            return null;
+        }
+        public void PrintAirlineFees()
+        {
+            Console.WriteLine($"{GetAirlineFromFlight}")
+        }
+
+    }
+}
