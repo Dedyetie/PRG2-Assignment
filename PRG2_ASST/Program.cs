14f1959bc87575966ede4fa06fb7a9b64c3ed058
Fierceyetie <s10267973@connect.np.edu.sg>
2025-01-16 09:22:25 +0800

    renamed project

--- PRG2_ASST/Program.cs
+++ PRG2_ASST/Program.cs
@@ -1,3 +1,14 @@
 ﻿using PRG2_ASST;
 
-Console.WriteLine("goodbye");
+Dictionary<string, Flight> FlightDict = new Dictionary<string, Flight>();
+using (StreamReader sr = new StreamReader("flights.csv"))
+{
+    sr.ReadLine();
+    string line = "";
+    while ((line = sr.ReadLine()) != null)
+    {
+        
+        string[] spline = line.Split(',');
+        FlightDict.Add(spline[0], new );
+    }
+}
